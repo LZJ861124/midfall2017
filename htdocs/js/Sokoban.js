578adc451e46e0731a5f389de0a171d17924b1e8
LZJ861124 <bii861124@gmail.com>
2017-11-24 14:14:05 +0800

    期中

---
+++ htdocs/js/Sokoban.js
@@ -0,0 +1,17 @@
+/**
+*  @file       index.js
+*  @brief      The entry file of Sokoban.
+*  @author     LZJ861124 (bii861124@gmail.com)
+*  @date       11/24/2017 created.
+*  @date       11/24/2017 last modified.
+*  @version    0.1.0
+*  @copyright  MIT, (C) 2017 LZJ861124
+*  @details
+*
+*  The entry file of Sokoban.
+*/
+'use strict';
+
+window.addEventListener('load', () => {
+  console.log("Sokoban.js loaded");
+});
